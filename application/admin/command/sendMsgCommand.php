3eebac887b49d6cbf8e271b352a4d49101a9d3f9
LiXianJun <602261903@qq.com>
2019-04-23 00:10:41 +0800

    1.0

--- application/admin/command/sendMsgCommand.php
+++ application/admin/command/sendMsgCommand.php
@@ -14,49 +14,94 @@ use think\console\Command;
 use think\console\Input;
 use think\console\Output;
 use think\Db;
+ini_set('max_execution_time','20');
+class sendMsgCommand extends Command
+{
 
-class sendMsgCommand extends Command {
-
-    protected function configure(){
+    protected function configure()
+    {
         $this->setName('sendMsgCommand')->setDescription("Acquiescence good evaluate");
     }
 
-    protected function execute(Input $input, Output $output){
-        $output->writeln('Date Crontab job start...');
+    public $obj;
+
+    protected function execute(Input $input, Output $output)
+    {
+        $output->writeln('send msg job start...');
         $this->start();
-//        $sendObj = new send();
-//        var_dump($sendObj->sendToMsg('李启德||33-4||义乌', 13301760258));
-        //"{"SendId":"2019042215512516306967214","InvalidCount":0,"SuccessCount":1,"BlackCount":0,"Code":0,"Message":"OK"}"
-        $output->writeln('Date Crontab job end...');
+        $output->writeln('send msg job end...');
     }
 
-    public function start(){
-        $start = 0;
-        $datas = $this->getNoPostDatas($start);
-        $sendObj = new send();
-        while ($datas){
-            foreach ($datas as $k => $v){
+    public function start()
+    {
+        Db::startTrans();
+        $datas = $this->getNoPostDatas();
+        while ($datas) {
+            foreach ($datas as $k => $v) {
                 $isMatched = preg_match('/(13\d|14[579]|15[^4\D]|17[^49\D]|18\d)+\d{8}/', $v['phone'], $matches);
-                if($isMatched){
-                    var_dump($v['phone']);
+                if ($isMatched) {
+                    if ($this->doSend($v['name'], $v['phone'], $v['number'], $v['city_id'])) {
+                        $this->_getTable()->where(['id' => $v['id']])->update(['is_post' => 1, 'post_date' => date('Y-m-d H:i:s')]);
+                    }
+                } else {
+                    $this->_getTable()->where(['id' => $v['id']])->delete();
                 }
             }
-            $start += 2;
-            $datas = $this->getNoPostDatas($start);
+            $datas = $this->getNoPostDatas();
         }
-
+        Db::commit();
+        return true;
     }
 
-    public function getNoPostDatas($start){
-        $datas = $this->_getTable()->field(['name','phone','city_id','number'])->where(['is_post' => 0])->limit($start,2)->select();
+    public function getNoPostDatas()
+    {
+        $datas = $this->_getTable()
+            ->field(['id', 'name', 'phone', 'city_id', 'number'])
+            ->where(['is_post' => 0])
+            ->limit(2)
+            ->select();
         return $datas;
     }
 
-
-    public function _getTable(){
-        return Db::table("send_msg");
+//     */10 * * * * php /www/wwwroot/www.pindi3088.cn/think sendMsgCommand >> /www/wwwroot/www.pindi3088.cn/runtime/command/sendMsg-$(date +\%Y-\%m-\%d).log
+    /**
+     * 发送短信函数
+     * @param $name
+     * @param $phone
+     * @param $number
+     * @param $city_id
+     */
+    public function doSend($name, $phone, $number, $city_id)
+    {
+        if ($city_id == 1) {
+            $city = '义乌';
+        } else {
+            $city = '上海';
+        }
+        $contend = "$name||$number||$city";
+        $result = $this->_sendObj()->sendToMsg($contend, $phone);
+        var_dump($result);
+        $results = json_decode($result, true);
+        if ($results['Message'] == 'OK') {
+            sleep(30);
+            echo "send to " . $contend . "\n";
+            Db::table("msg_log")->insert(['phone' => $phone, 'msg' => $contend, 'created_at' => date('Y-m-d H:i:s')]);
+            return true;
+        } else {
+            sleep(30);
+            return false;
+        }
     }
 
+    public function _sendObj()
+    {
+        return $this->obj = new send();
+    }
+
+    public function _getTable()
+    {
+        return Db::table("send_msg");
+    }
 
 
 }
